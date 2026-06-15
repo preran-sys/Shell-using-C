e64020e090721965ecaaab675072983a9d96f0ee
preran-sys <polarisf@student.ubc.ca>
2026-06-15 15:48:16 -0700

    Implement type command

--- main.c
+++ main.c
@@ -71,8 +71,27 @@ void execute_user_input(char command[LIMIT]) {
     // Verify echo to be the command argument 0
     if (strcmp(command, "echo") == 0) {
         printf("");
-    } else if (strncmp(command, "echo ", 5) == 0) {
+    }
+    if (strncmp(command, "echo ", 5) == 0) {
         printf("%s\n", command + 5);
+    }
+    // Implement type
+    // Verify type to be the command argument 0
+    if (strcmp(command, "type") == 0) {
+        printf("");
+    }
+    if (strncmp(command, "type ", 5) == 0) {
+        // Validate builtin shell commands to print "<command> is a shell builtin"
+        if (strcmp(command + 5, "echo") == 0) {
+            printf("%s is a shell builtin\n", command + 5);
+        } else if (strcmp(command + 5, "exit") == 0) {
+            printf("%s is a shell builtin\n", command + 5);
+        } else if (strcmp(command + 5, "type") == 0) {
+            printf("%s is a shell builtin\n", command + 5);
+        } else {
+            // Print invalid command format "<command>: not found"
+            printf("%s: not found\n", command + 5);
+        }
     } else {
         // Print invalid command format "<command>: command not found"
         printf("%s: command not found\n", command);
@@ -81,6 +100,7 @@ void execute_user_input(char command[LIMIT]) {
 
 
 char * delimit(char command[LIMIT], char * delimiter) {
+
     return NULL;
 }
 
